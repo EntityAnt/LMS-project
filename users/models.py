2855b3aff98c952758bfc1f766b62ff5f78c970c
Anton Ershov <ant_e@mail.ru>
2024-11-20 12:26:17 +0500

    add black

--- users/models.py
+++ users/models.py
@@ -7,19 +7,31 @@ NULLABLE = {"blank": True, "null": True}
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(unique=True, verbose_name='Почта', help_text='Укажите почту')
-    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='Телефон', help_text='Укажите номер телефона')
-    tg_nick = models.CharField(max_length=50, **NULLABLE, verbose_name='ТГ-ник', help_text='Укажите ТГ ник')
-    city = models.CharField(max_length=100, **NULLABLE, verbose_name='Город', help_text='Укажите город')
-    avatar = models.ImageField(upload_to='users/avatars', **NULLABLE, verbose_name='Аватар',
-                               help_text='Загрузите фото аватара')
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        **NULLABLE,
+        verbose_name="Телефон",
+        help_text="Укажите номер телефона"
+    )
+    tg_nick = models.CharField(
+        max_length=50, **NULLABLE, verbose_name="ТГ-ник", help_text="Укажите ТГ ник"
+    )
+    city = models.CharField(
+        max_length=100, **NULLABLE, verbose_name="Город", help_text="Укажите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        **NULLABLE,
+        verbose_name="Аватар",
+        help_text="Загрузите фото аватара"
+    )
 
-
-
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
